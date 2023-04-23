bdb7e7e6c535d5a8f45f88581fee5e5ad8dab0e3
sbojorge <sara.bojorge@gmail.com>
2023-04-23 22:16:00 +0200

    create the recipe model

---
+++ recipes/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Recipe
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "meal_type",
+        "calories",
+        "instructions",
+        "ingredients",
+        "image",
+    )
+    list_filter = ("meal_type",)
