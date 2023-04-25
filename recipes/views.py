e1e448becc175d44b2487e5f1f456cd5daf5e061
sbojorge <sara.bojorge@gmail.com>
2023-04-25 11:01:03 +0200

    create the AddRecipe model, its view, url and template

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from .models import Recipe
+from .forms import RecipeForm
 
-# Create your views here.
+
+class AddRecipe(CreateView):
+    """Add recipe view"""
+
+    template_name = "recipes/add_recipe.html"
+    model = Recipe
+    form_class = RecipeForm
+    success_url = "/recipes/"
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(AddRecipe, self).form_valid(form)
